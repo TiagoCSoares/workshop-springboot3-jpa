d1148f2407a53e61d0c2a94fc8537b891d4cf7c9
TiagoCSoares <tiagocsoares22@gmail.com>
2024-02-15 21:44:21 -0300

    Service layer, component registration

--- course/src/main/java/com/tiagocsoares/course/resources/UserResource.java
+++ course/src/main/java/com/tiagocsoares/course/resources/UserResource.java
@@ -1,18 +1,31 @@
 package com.tiagocsoares.course.resources;
 
 import com.tiagocsoares.course.entities.User;
+import com.tiagocsoares.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController // uma classe que manipula solicitações HTTP e retorna respostas no formato de dados que não são vinculados a uma visualização específica, como JSON ou XML.
 @RequestMapping(value= "/users") // mapeia solicitações da web para classes de manipulador e métodos de manipulador específicos.
 public class UserResource {
+    @Autowired
+    private UserService service;
 
     @GetMapping // Esta anotação indica que este método é mapeado para uma solicitação HTTP GET para a rota especificada.
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
